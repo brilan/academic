4b972ee9a66f9bd244f665b226087e3d3dfb9402
sagansdoom <34175987+sagansdoom@users.noreply.github.com>
2018-11-21 15:51:07 -0800

    add input from file and final testing

--- cis292-data-structures/Labs/sudoku/TestInput.java
+++ cis292-data-structures/Labs/sudoku/TestInput.java
@@ -4,30 +4,53 @@ import java.util.Scanner;
 
 public class TestInput {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) 
+        throws FileNotFoundException {
 
-        Scanner scan = null;
+        Scanner scan = new Scanner( System.in );
 
-        int[] testArray = new int[ 9 ];
+        int[][] testArray = new int[ 9 ][ 9 ];
 
-        // open file
-        try {
+        boolean validFile = false; // valid file flag
+        int counter = 0; // to track invalid file entries
+        while( !validFile ) {
+            try {
+                if( 0 < args.length ) {
+                    scan = new Scanner( new File( args[ 0 ] ) );
+                } else {
+                    System.out.print( "Enter name of hint file: " );
+                    String filename = scan.nextLine();
 
-            scan = new Scanner( new File( args[ 0 ] ) );
+                    scan = new Scanner( new File( filename ) );
+                }
+                validFile = true;
+            } catch( FileNotFoundException e ) {
+                System.out.printf("File not found!%n%n");
+            }
 
-        } catch( FileNotFoundException e ) {
-            e.printStackTrace();
+            // increment counter and exit program after three invalid file entries 
+            counter++;
+            if( counter > 2 ) {
+                System.out.println( "File not found. Please try again." );
+                System.exit(0);
+            }
         }
 
         // read values from file
-        for( int i = 0; i < testArray.length; i++ ) {
-
-            testArray[ i ] = scan.nextInt();
-
+        for( int i = 0; i < 9; i++ ) {
+            for( int j = 0; j < 9; j++) {
+                testArray[ i ][ j ] = scan.nextInt();
+            }
         }
 
-        for( int i = 0; i < testArray.length; i++ ) {
-            System.out.print( testArray[ i ] + " " );
+        // print values
+        System.out.println(); // insert blank line
+        for( int i = 0; i < 9; i++ ) {
+            for( int j = 0; j < 9; j++) {
+                System.out.print( testArray[ i ][ j ] + " " );
+                if( j == 8 )
+                    System.out.println();
+            }
         }
 
     }
